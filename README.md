ca7dac117a5b196811253d31ed7bddc522c5c9f6
Orhan Maden <omaden@cyclomedia.com>
2019-03-07 01:35:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# spotify-resource-cleaner-windows
+Cleans the Storage, Data and Browser folders in %localappdata%\Spotify.
+
+[![Build Status](https://www.travis-ci.org/orhanmaden/spotify-resource-cleaner-windows.svg?branch=master)](https://www.travis-ci.org/orhanmaden/spotify-resource-cleaner-windows)
+
+Supported platforms: Windows
+
+Goal: Make spotify faster by clearing the local app data folders
+
+Source: https://community.spotify.com/t5/Desktop-Windows/Why-is-my-spotify-often-so-slow/m-p/1285274#M17673
---
+++ SpotifyResourceCleaner/Program.cs
@@ -0,0 +1,65 @@
+﻿using CommandLine;
+using System;
+using System.IO;
+
+namespace SpotifyResourceCleaner
+{
+  class Program
+  {
+    public class Options
+    {
+      [Option('f', "folder", Required = false, HelpText = "Set Spotify('%localappdata%') folder.")]
+      public string Folder { get; set; }
+    }
+
+    private static readonly string[] FoldersToDelete = { "Storage", "Data", "Browser" };
+
+    private static void Main(string[] args)
+    {
+      Parser.Default
+        .ParseArguments<Options>(args)
+        .WithParsed(Run);
+    }
+
+    static void Run(Options options)
+    {
+      var localSpotifyFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Spotify");
+      if (!string.IsNullOrWhiteSpace(options.Folder))
+        localSpotifyFolderPath = options.Folder;
+
+      if (!Directory.Exists(localSpotifyFolderPath))
+      {
+        WriteLine($"Spotify folder at path '{localSpotifyFolderPath}' doesn't exist.");
+        Exit();
+      }
+
+      foreach (var folderToDelete in FoldersToDelete)
+      {
+        var folderToDeletePath = Path.Combine(localSpotifyFolderPath, folderToDelete);
+        if (Directory.Exists(folderToDeletePath))
+        {
+          WriteLine($"Cleaning folder {folderToDeletePath}...");
+          Directory.Delete(folderToDeletePath, true);
+          Directory.CreateDirectory(folderToDeletePath);
+          WriteLine($"Cleaned folder {folderToDeletePath}.");
+        }
+        else
+        {
+          WriteLine($"Skipping folder {folderToDeletePath} because it doesn't exits.");
+        }
+      }
+      Exit();
+    }
+
+    private static void WriteLine(string message)
+    {
+      Console.WriteLine(message);
+    }
+
+    private static void Exit()
+    {
+      WriteLine("Press enter to exit...");
+      Console.Read();
+    }
+  }
+}
